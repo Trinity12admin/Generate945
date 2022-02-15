67cfe6573c765be5722aa261026283d4b950b575
Mr_Natel <mr.natel@att.net>
2022-02-15 12:23:03 -0600

    updated assembly title to reflect the program

--- DropShipTools/Properties/AssemblyInfo.cs
+++ DropShipTools/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChannelAdvisorInventory")]
+[assembly: AssemblyTitle("EDI End of Day")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChannelAdvisorInventory")]
+[assembly: AssemblyProduct("EDI End of Day")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
